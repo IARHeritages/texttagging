e7beefa251fb828a189d9f07a0ecde20fd2e09b7
Daniel Lombraña González <teleyinex@gmail.com>
2016-12-14 13:19:17 +0100

    Crawler

---
+++ getData.py
@@ -0,0 +1,37 @@
+import scrapy
+import settings
+
+class QuotesSpider(scrapy.Spider):
+    name = settings.NAME
+    start_urls = settings.START_URLS
+
+    def parse(self, response):
+        for href in response.css('div.body>h2>a::attr("href")').extract():
+            if (settings.DOMAIN) in href:
+                yield scrapy.Request(response.urljoin(href),
+                                     callback=self.parse_article)
+
+        next_page = response.css('li.pager-next a::attr("href")').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+
+    def parse_article(self, response):
+        main_text = ''
+        title = ''
+        time = ''
+        author = ''
+
+        for t in response.css('header>h1'):
+            title = t.css('h1::text').extract_first()
+        for p in response.css('div.text-wrapper>p'):
+            tmp =  p.css('p').extract_first()
+            if tmp:
+                main_text += '\n ' + tmp
+        for a in response.css('li.author>span>a'):
+            author = a.css('a::text').extract_first()
+        for t in response.css('time'):
+            time = t.css('time::attr("datetime")').extract_first()
+        yield {'text': main_text, 'title': title,
+               'author': author, 'datetime': time, 'url': response.url,
+               'source': settings.SOURCE}
